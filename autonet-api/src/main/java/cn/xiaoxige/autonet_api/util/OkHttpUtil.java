1ca43155b75137e8028986348933142fad942086
zhuxiaoan <zhuxiaoan@hxkid.com>
2017-12-02 15:12:32 +0800

    develop update update 使okhttp支持默认认证的https

--- autonet-api/src/main/java/cn/xiaoxige/autonet_api/util/OkHttpUtil.java
+++ autonet-api/src/main/java/cn/xiaoxige/autonet_api/util/OkHttpUtil.java
@@ -2,7 +2,17 @@ package cn.xiaoxige.autonet_api.util;
 
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import cn.xiaoxige.autonet_api.config.AutoNetConfig;
 import cn.xiaoxige.autonet_api.interfaces.IAutoNetEncryptionCallback;
@@ -20,6 +30,8 @@ public class OkHttpUtil {
                                      long writeTime, long readtime, long connectOutTime,
                                      IAutoNetEncryptionCallback autoNetEncryptionCallback) {
         OkHttpClient client = new OkHttpClient.Builder()
+                .sslSocketFactory(createSSLSocketFactory())
+                .hostnameVerifier(new TrustAllHostnameVerifier())
                 .addNetworkInterceptor(new BaseApplicationInterceptor(isencryption, mencryptionkey, config, autoNetEncryptionCallback))
                 .addNetworkInterceptor(new StethoInterceptor())
                 .writeTimeout(writeTime, TimeUnit.SECONDS)
@@ -28,4 +40,41 @@ public class OkHttpUtil {
                 .build();
         return client;
     }
+
+    private static class TrustAllCerts implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+    private static class TrustAllHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new TrustAllCerts()}, new SecureRandom());
+
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+        }
+
+        return ssfFactory;
+    }
+
 }
